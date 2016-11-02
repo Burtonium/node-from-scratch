ced2da079d1041b7c6e1e907387cfe6c3d2e9173
Burtonium <madeofburtonium@gmail.com>
2016-11-02 05:49:00 +0000

    users migration and seed

---
+++ db/seeds/development/users_seed.js
@@ -0,0 +1,22 @@
+exports.seed = function(knex, Promise) {
+  var t = 'users';
+  var users = [];
+  
+  for (var i = 0; i < 100; ++i) {
+    users.push(
+      knex(t).insert({
+        username: 'user' + i,
+        email: 'user' + i + '@burtonize.me',
+        password: ''
+      })
+    );
+  }
+
+  // Delete existing entries
+  return knex(t).del()
+  
+    // then insert seeded data
+    .then(function() {
+      return Promise.all(users);
+    });
+};
---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+  
+  development: {
+    client: 'postgres',
+    connection: {
+      database: 'brtnzr_db',
+      user:     'ubuntu',
+      password: 'ayylmao'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    }
+  }
+};
